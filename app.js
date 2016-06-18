4dc4602229ced13b92d914442dea0d063eaf2fc0
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-18 00:04:14 -0500

    Update app routes for new models

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var dailyRidershipRoute = require('./routes/daily-ridership');
+var routesHandler = require('./routes/routes');
+var systemTrendsRoutes = require('./routes/system-trends');
+var dailyRidershipRoutes = require('./routes/daily-riderships');
+var serviceHourRidershipRoutes = require('./routes/service-hour-riderships');
 
 var app = express();
-
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -18,9 +20,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/daily-ridership', dailyRidershipRoute);
+app.use('/routes', routesHandler);
+app.use('/system-trends', systemTrendsRoutes);
+app.use('/daily-riderships', dailyRidershipRoutes);
+app.use('/service-hour-riderships', serviceHourRidershipRoutes);
 
-// catch 404 and forward to error handler
+// catch 404 for a request without a resource route
+// and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
@@ -33,22 +39,28 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.json({
-      message: err.message,
-      error: err
-    });
+    var statusCode = err.status || 500;
+    res.status(statusCode);
+    var jsonapiError = {
+      'status': new String(statusCode),
+      'detail': err.message
+    }
+    var errors = [jsonapiError];
+    res.json({ 'errors': errors });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: {}
-  });
+  var statusCode = err.status || 500;
+  res.status(statusCode);
+  var jsonapiError = {
+    'status': new String(statusCode),
+    'detail': err.message
+  }
+  var errors = [jsonapiError];
+  res.json({ 'errors': errors });
 });
 
 
