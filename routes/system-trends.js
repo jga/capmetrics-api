0b05d2ce5158b966ff3ecdf82fe585181846b033
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-06-30 15:06:39 -0500

    Create performance and plain collection handler for system trends requests.

--- routes/system-trends.js
+++ routes/system-trends.js
@@ -6,7 +6,7 @@ var toDocument = require('../utils/to-document');
 
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+var handleDirectQuery = function(models, res, next) {
   models.SystemTrend
     .findAll()
     .then(function(systemTrends) {
@@ -25,6 +25,29 @@ router.get('/', function(req, res, next) {
     .catch(function(error) {
       next(error);
     })
+}
+
+var handleCollection = function (models, res, next) {
+  models.PerformanceDocument
+    .findAll({where: {'name': 'system-trends'}})
+    .then(function(performanceDocument) {
+      if (performanceDocument) {
+        res.type('application/vnd.api+json');
+        res.json(JSON.parse(performanceDocument.document));
+      } else {
+        res.json({'data': []});
+      }
+    });
+}
+
+router.get('/', function(req, res, next) {
+  // check for performance mode to be set to off
+  if ('performance' in req.query && req.query.performance === 'off') {
+    handleDirectQuery(models, res, next);
+  // query not found, send to high performance handler
+  } else {
+    handleCollection(models, res, next);
+  }
 });
 
 module.exports = router;
