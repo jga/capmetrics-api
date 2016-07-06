ff6fe81de2004a6f5d0f3ccb7e9d6ad5269799db
Julio Gonzalez Altamirano <devjga@gmail.com>
2016-07-06 12:07:26 -0500

    Add high ridership route.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 
+var highRidershipRoutes = require('./routes/high-ridership')
 var routesHandler = require('./routes/routes');
 var routeLabelsHandler = require('./routes/route-labels');
 var systemTrendsRoutes = require('./routes/system-trends');
@@ -26,11 +27,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/daily-riderships', dailyRidershipRoutes);
+app.use('/high-ridership', highRidershipRoutes);
 app.use('/routes', routesHandler);
 app.use('/route-labels', routeLabelsHandler);
-app.use('/system-trends', systemTrendsRoutes);
-app.use('/daily-riderships', dailyRidershipRoutes);
 app.use('/service-hour-riderships', serviceHourRidershipRoutes);
+app.use('/system-trends', systemTrendsRoutes);
 
 // catch 404 for a request without a resource route
 // and forward to error handler
---
+++ routes/high-ridership.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var models = require('../models');
+var express = require('express');
+
+var router = express.Router();
+
+
+router.get('/', function(req, res, next) {
+  models.PerformanceDocument
+    .findAll({ where: { name: 'top-routes' } })
+    .then(function(performanceDocuments) {
+      if (performanceDocuments[0]) {
+        res.type('application/vnd.api+json');
+        let highRidershipRoutes = JSON.parse(performanceDocuments[0].document);
+        res.json(highRidershipRoutes);
+      } else {
+        res.json([]);
+      }
+    });
+})
+
+module.exports = router;
